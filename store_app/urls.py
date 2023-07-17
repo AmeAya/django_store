31d6a1d2c2d9ef301d26674b4b2581a9881bd124
AmeAya <ameaya@MacBook-Air-AmeAya.local>
2023-07-17 20:21:06 +0600

    update user data

--- store_app/urls.py
+++ store_app/urls.py
@@ -6,4 +6,5 @@ urlpatterns = [
     path('home', homeView, name='home_url'),
     path('get_goods_by_category/<int:category_pk>', getGoodsByCategory, name='goods_by_category_url'),
     path('good_detail/<int:good_pk>', goodDetail, name='good_detail'),
+    path('cabinet', cabinetView, name='cabinet'),
 ]
--- store_app/views.py
+++ store_app/views.py
@@ -1,27 +1,10 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import *
 
 
 def homeView(request):  # Каждая view должна принимать request
-    user = request.user  # request.user - это наш user
-
-    # customer = Customer.objects.all()
-    # # Выгрузить все данные из таблицы Customer
-    # print(customer)
-    #
-    # customer = Customer.objects.filter(user=user)
-    # # Выгрузить записи у которых user это наш user
-    # print(customer)
-
-    customer = Customer.objects.get(user=user)
-    # Выгрузить только одну запись у которой user это наш user
-    # Используйте get когда берем только 1 запись (Желательно по id/pk)
-    # print(customer)
-
     categories = Category.objects.all()
-    context = {'Customer': customer,
-               'Message': 'I am Alive',
-               'Categories': categories}
+    context = {'Categories': categories}
     # context - Данные которые возвращаются на html
     # context обязан быть словарем (dictionary)
     return render(request, 'home.html', context)
@@ -37,3 +20,26 @@ def goodDetail(request, good_pk):
     good = Good.objects.get(pk=good_pk)
     context = {'good': good}
     return render(request, 'good_detail.html', context)
+
+
+def cabinetView(request):
+    customer = Customer.objects.get(user=request.user)
+    if request.method == 'GET':
+        context = {
+            'Customer': customer,
+            'Message': 'I am Alive'
+        }
+        return render(request, 'cabinet.html', context)
+    else:  # request.method == 'POST'
+        phone = request.POST.get('phone_name')
+        username = request.POST.get('username_name')
+        if 'photo_name' in request.FILES:
+            photo = request.FILES['photo_name']
+            customer.photo = photo
+        if phone:
+            customer.phone = phone
+        if username:
+            request.user.username = username
+            request.user.save()
+        customer.save()
+        return redirect('cabinet')
